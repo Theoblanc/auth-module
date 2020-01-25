8bef2ea72fb7df72e7ab16cdd358bbb8e60de635
TheoBlanc <jtho1015@gmail.com>
2020-01-25 11:02:20 +0900

    Form & Queries

--- auth-module-client/src/components/login/LoginContainer.tsx
+++ auth-module-client/src/components/login/LoginContainer.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import LoginPresenter from "./LoginPresenter";
+import { useForm } from "react-hook-form";
+
 const LoginContainer: React.FC = () => {
-  return <LoginPresenter />;
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <LoginPresenter
+      onSubmit={onSubmit}
+      register={register}
+      handleSubmit={handleSubmit}
+    />
+  );
 };
 
 export default LoginContainer;
