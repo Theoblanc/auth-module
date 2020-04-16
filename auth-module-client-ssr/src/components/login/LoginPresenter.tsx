a2036448892fa17edfe4f4f3d9b91888ea5616c2
TheoBlanc <jtho1015@gmail.com>
2020-04-16 11:35:08 +0900

    :star: new: add login component

---
+++ auth-module-client-ssr/src/components/login/LoginPresenter.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface IProps {
+  onSubmit: Function;
+  register: any;
+  handleSubmit: Function;
+}
+
+const LoginPresenter: React.FC<IProps> = ({
+  register,
+  handleSubmit,
+  onSubmit
+}) => {
+  return (
+    <div>
+      <div>Login 페이지</div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="login"
+          name="email"
+          ref={register}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          ref={register}
+        ></input>
+        <input type="submit"></input>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPresenter;
