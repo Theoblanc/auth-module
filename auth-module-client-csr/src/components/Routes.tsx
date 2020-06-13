c6d5d03121ca2548be21044e40eed90607f7f636
TheoBlanc <jtho1015@gmail.com>
2020-06-14 02:23:16 +0900

    :star: new: change files

--- auth-module-client-csr/src/components/Routes.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "../pages/Login";
-
-export default () => (
-  <Router>
-    <Route path="/" component={Login} />
-  </Router>
-);
--- auth-module-client-csr/src/styles/theme.ts
+++
@@ -1,8 +0,0 @@
-const theme = {
-  blackColor: "#202225",
-  darkGrayColor: "#2f3136",
-  grayColor: "#36393f",
-  chatUesrNameColor: "#72766f"
-};
-
-export default theme;
